51da053e11a0ac9953412db2ea6f77157b224cb2
Estefanía Garcés Pérez <1020egp@gmail.com>
2021-11-07 13:12:56 -0500

    Add: initial commit of project. EGP.

---
+++ Flask/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, make_response, redirect, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect('/hello'))
+    response.set_cookie('ip', user_ip)
+    return response
+
+
+@app.route("/hello")
+def hello():
+    user_ip=request.cookies.get('ip')
+    return f"Hello world, tu ip es {user_ip}"
+
+DEBUG = True
+HOST = 4000
+""" if __name__ = "__main__":
+    app.run() """
