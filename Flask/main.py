6cf11c0a21700be44c0a6d07caa0a205c394beb2
Estefanía Garcés Pérez <1020egp@gmail.com>
2021-11-08 16:04:44 -0500

    Changes: file localizations. EGP.

--- Flask/main.py
+++ Flask/main.py
@@ -1,22 +1,12 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from flask import request, make_response, redirect, render_template, session, url_for, flash
+
 import unittest
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+from app import create_app
+from app.forms import LoginForm
 
-app.config['SECRET_KEY'] = 'SUPER SECRETO'
+app = create_app()
 todos = ['Comprar cafe.', 'Programar.', 'Pedir disculpas por la entrega tarde.']
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
-
 
 @app.cli.command()
 def test():
@@ -58,8 +48,3 @@ def hello():
         return redirect(url_for('index'))
 
     return render_template('hello.html', **context)
-
-DEBUG = True
-HOST = 4000
-""" if __name__ = "__main__":
-    app.run() """
