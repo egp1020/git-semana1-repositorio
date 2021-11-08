5420f159b04773cbe280e0ce73d82d41683f60c9
Estefanía Garcés Pérez <1020egp@gmail.com>
2021-11-08 15:36:58 -0500

    Add: Alert message and unit test. EGP.

--- Flask/main.py
+++ Flask/main.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+import unittest
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -15,6 +16,12 @@ class LoginForm(FlaskForm):
     username = StringField('Nombre de usuario', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
+
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
 
 @app.errorhandler(404)
 def not_found(error):
@@ -47,6 +54,7 @@ def hello():
     if login_form.validate_on_submit():
         username = login_form.username.data
         session["username"] = username
+        flash('Nombre de ususario registrado con éxito.')
         return redirect(url_for('index'))
 
     return render_template('hello.html', **context)
