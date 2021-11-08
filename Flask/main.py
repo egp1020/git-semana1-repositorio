b1abe45adf109343d1fd7b05460fc76cddabfcc7
Estefanía Garcés Pérez <1020egp@gmail.com>
2021-11-08 14:06:03 -0500

    add: working with WTForms.

--- Flask/main.py
+++ Flask/main.py
@@ -1,11 +1,20 @@
 from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 todos = ['Comprar cafe.', 'Programar.', 'Pedir disculpas por la entrega tarde.']
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -26,9 +35,11 @@ def index():
 def hello():
     user_ip=session.get('ip')
     # return render_template('hello.html', user_ip = user_ip, todo = todos)
+    login_form=LoginForm()
     context={
         'user_ip':user_ip,
         'todos': todos,
+        'login_form':login_form,
     }
     return render_template('hello.html', **context)
 
