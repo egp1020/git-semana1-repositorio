7859e26beeda6ac09cedfc6df143419ad5608693
Estefanía Garcés Pérez <1020egp@gmail.com>
2021-12-27 16:59:26 -0500

    Add: clases del curso básico de JavaScript de Platzi. EGP.

---
+++ 06-Curso-Basico-JavaScript/01.js
@@ -0,0 +1,12 @@
+/*
+    Mostrando el tipado débil que tiene javascript
+*/
+
+datoUno = 4; // tipo number
+datoDos = "7"; // tipo string
+datoCuatro = true; // tipo boolean - 1
+datoCinco = false; // tipo boolean - 0
+console.log(datoUno + datoDos); // 47
+console.log(datoUno*datoDos); // 28
+console.log(datoUno + datoCuatro) // 5
+console.log(datoCinco - 3) // -3
---
+++ 06-Curso-Basico-JavaScript/02.js
@@ -0,0 +1,35 @@
+/* Tipos de datos */
+
+// Valores primitivos
+
+40 // Valor tipo número
+"40" // Valor tipo string
+
+/* Valores para tomar decisiones */
+true // Valor tipo booleano George Boole
+false // Valor tipo booleano
+
+/* Valores para reservar o producto de un error*/
+null // Valor tipo nulo
+undefined // Valor tipo indefinido
+
+// Valores no primitivos u objetos
+[1, 2, 3, 4] // Valor tipo arreglo
+{nombre: "Estefanía"} // Valor tipo objeto
+
+// Para saber el tipo de dato de un valor
+console.log(typeof 40); // number
+console.log(typeof(40)); // number
+console.log(typeof "40"); // string
+console.log(typeof true); // boolean
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof [1, 2, 3, 4]); // object
+console.log(typeof {nombre: "Estefanía"}); // object
+
+
+
+
+
+
+
